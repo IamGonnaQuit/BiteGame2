1b0dc2d2754af94a143bef3dc46b9ee8ba8769db
Stefan Mandita <damsteffy2000@gmail.com>
2025-09-04 14:02:07 +0200

    FIX

--- Assets/Hazard.cs
+++ Assets/Hazard.cs
@@ -1,21 +1,41 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Hazard : MonoBehaviour
 {
-    public int damageAmount = 10; 
-    public string targetTag = "Player"; 
+    public int damageAmount = 10;
+    public bool isActive = true;   // hazard starts active by default
+    [SerializeField] private float minTime = 3f;
+    [SerializeField] private float maxTime = 4f;
 
-    private void OnTriggerEnter(Collider other)
+    private void Start()
     {
-        if (other.CompareTag(targetTag))
-        {
-            BloodMeter dmg = other.GetComponent<BloodMeter>();
+        StartCoroutine(ToggleRoutine());
+    }
 
-            if (dmg != null)
-            {
-                dmg.DealingDamage(damageAmount);
-            }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!isActive) return; // do nothing if hazard is "off"
+
+        BloodMeter dmg = other.GetComponent<BloodMeter>();
+        if (dmg != null)
+        {
+            dmg.DealingDamage(damageAmount); // make sure your BloodMeter has this method
+        }
+    }
+
+    private System.Collections.IEnumerator ToggleRoutine()
+    {
+        while (true)
+        {
+            // flip active state
+            isActive = !isActive;
+
+            // (optional) make the object visually disappear when inactive
+            gameObject.GetComponent<SpriteRenderer>().enabled = isActive;
+
+            // wait random 3–4 seconds
+            float waitTime = Random.Range(minTime, maxTime);
+            yield return new WaitForSeconds(waitTime);
         }
     }
 }
