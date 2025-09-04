d7bbf487eef5bffec90032d5e3a336f133309f8e
Dryskulls-Leon <lkf2209@icloud.com>
2025-09-04 11:53:24 +0200

    mosquito sucking

--- Assets/Scripts/Mosquito Movement.cs
+++ Assets/Scripts/Mosquito Movement.cs
@@ -8,13 +8,6 @@ public class MosquitoMovement : MonoBehaviour
     private Vector3 mousePosition;
     [SerializeField]
     private float mosquitoSpeed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         mousePosition = Input.mousePosition;
